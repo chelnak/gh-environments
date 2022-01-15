32d3ee6f12638a6f8ab5577ba89b838254abd0b7
Craig Gumbley <craiggumbley@gmail.com>
2022-01-15 16:54:40 +0000

    Temporary buildscript override
    Enable a way to optionally set ldflags to setversion variable
    inside the binary.

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+tag=$(git describe --tags --abbrev=0)
+platforms=$(echo "darwin-amd64,linux-386,linux-arm,linux-amd64,linux-arm64,windows-386,windows-amd64" | tr "," "\n")
+include="dist/*"
+
+if [ -n "${GH_EXT_BUILD_SCRIPT}" ]; then
+  echo "invoking build script override ${GH_EXT_BUILD_SCRIPT}"
+  ./${GH_EXT_BUILD_SCRIPT} $tag || exit $?
+else
+
+  if [ -n "${GH_VERSION_SPEC}" ]; then
+    # build the ldflags arg in two parts due to this limitation:
+    # https://unix.stackexchange.com/questions/502715/escape-parameters-for-positional-arguments
+    LDFLAG=-ldflags
+    VERSION_SPEC="-X ${GH_VERSION_SPEC}=${tag}"
+  fi
+
+  for p in $platforms; do
+    goos=$(echo $p | sed 's/-.*//')
+    goarch=$(echo $p | sed 's/.*-//')
+    ext=""
+    if [[ "${goos}" == "windows" ]]; then
+      ext=".exe"
+    fi
+    GOOS=${goos} GOARCH=${goarch} go build -o "dist/${goos}-${goarch}${ext}" ${LDFLAG} "${VERSION_SPEC}"
+  done
+fi
+
+ls -A dist >/dev/null || (echo "no files found in dist/" && exit 1)
+
+if [ -n "${GPG_FINGERPRINT}" ]; then
+  for f in $(ls dist); do
+    shasum -a 256 dist/$f >> checksums.txt
+  done
+  gpg --output checksums.txt.sig --detach-sign checksums.txt
+  include="dist/* checksums*"
+fi
+
+prerelease=""
+
+if [[ "${tag}" =~ .*-.* ]]; then
+  prerelease="-p"
+fi
+
+gh api repos/$GITHUB_REPOSITORY/releases/generate-notes \
+  -f tag_name="${tag}" -q .body > CHANGELOG.md
+
+gh release create $tag $prerelease --notes-file CHANGELOG.md $include

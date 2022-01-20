3f68d2e3561ccd6c69f6fa5e0ea0082a94fb8352
Craig Gumbley <craiggumbley@gmail.com>
2022-01-20 17:55:20 +0000

    Add env alias
    Sometimes it's quicker to use env...

--- cmd/root.go
+++ cmd/root.go
@@ -11,6 +11,7 @@ var version = "dev"
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "environments [command]",
+	Aliases: []string{"env"},
 	Short:   "Work with GitHub environments",
 	Long:    "Work with GitHub environments",
 	Version: version,

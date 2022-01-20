bf79642978fbac5423663d23419a5e5723f7c280
Craig Gumbley <craiggumbley@gmail.com>
2022-01-20 18:21:37 +0000

    rb

--- internal/client/environments.go
+++ internal/client/environments.go
@@ -1,8 +1,3 @@
-// Copyright 2021 The go-github AUTHORS. All rights reserved.
-//
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package client
 
 import (

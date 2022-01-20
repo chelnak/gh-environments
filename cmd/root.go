6ad8c02668c50df2b68ae62358b0a46b2bea0f0a
Craig Gumbley <craiggumbley@gmail.com>
2022-01-20 21:20:35 +0000

    Remove cmd alias
    Setting an alias on the root command doesn't
    seem to work as expected.

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,6 @@ var ErrSilent = errors.New("ErrSilent")
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:           "environments [command]",
-	Aliases:       []string{"env"},
 	Short:         "Work with GitHub environments",
 	Long:          "Work with GitHub environments",
 	Version:       version,
